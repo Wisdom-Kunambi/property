23abb5deb72e0395446dd17107796681901748c1
Wisdom-Kunambi <kunambiwaziri1@gmail.com>
2024-04-16 17:37:07 +0300

    Add Residence Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Companies from "./components/Companies/Companies";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Residencies from "./components/Residencies/Residencies";
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
       <Companies />
+      <Residencies />
     </div>
   );
 }

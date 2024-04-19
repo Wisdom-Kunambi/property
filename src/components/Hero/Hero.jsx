f49e63350c007cdd026eb98253c0c9fcf7827b06
Wisdom-Kunambi <kunambiwaziri1@gmail.com>
2024-04-19 17:19:25 +0300

    Add motion 'frame-motion' in the Hero Component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
+import { motion } from "framer-motion";
 
 const Hero = () => {
     return (
@@ -11,9 +12,13 @@ const Hero = () => {
                 <div className="flexColStart hero-left">
                     <div className="hero-title">
                         <div className="orange-circle" />
-                        <h1>
+                        <motion.h1
+                        initial={{y: "2rem", opacity: 0}}
+                        animate={{y: 0, opacity: 1}}
+                        transition={{type: "spring", duration: 2}}
+                        >
                             Discover <br /> Most Suitable <br /> Property
-                        </h1>
+                        </motion.h1>
                     </div>
 
                     <div className="flexColStart hero-des">
@@ -55,9 +60,13 @@ const Hero = () => {
                 </div>
                 {/* Right Side */}
                 <div className="flexCenter hero-right">
-                    <div className="image-container">
+                    <motion.div 
+                    initial={{x: "7rem", opacity: 0}}
+                    animate={{x: 0, opacity: 1}}
+                    transition={{type: "spring", duration: 2}}
+                    className="image-container">
                         <img src="./hero-image.png" alt="" />
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </section>
